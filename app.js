079ced715fd8362d180e2871da56236cb711e9f1
Johan Higuita <johanhiguita1146739@correo.itm.edu.co>
2026-04-21 16:42:13 -0500

    revert app js

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.get("/api/respuestas", (req, res) => {
-  res.json([
-    { p1: 1, p2: 0, p3: 1 },
-    { p1: 1, p2: 1, p3: 0 }
- ]);
-});
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('@routes'));
 
 module.exports = app;
